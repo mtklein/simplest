3a5deac6e4a0957dea42476f6e01504b173422da
Mike Klein <mtklein@gmail.com>
2024-02-19 13:13:18 -0600

    some ideas

--- simplest.c
+++ simplest.c
@@ -1,11 +1,13 @@
 #include "simplest.h"
+#include <stdint.h>
+#include <string.h>
 
 define_step(noop) {
-    return next(ip,x,y);
+    return call(st+1,x,y);
 }
 
 define_step(swap_rb) {
-    RGBA c = next(ip,x,y);
+    RGBA c = call(st+1,x,y);
 
     Half tmp;
     tmp = c.r;
@@ -15,7 +17,7 @@ define_step(swap_rb) {
 }
 
 define_step(grad) {
-    half const *alpha = ip->ctx;
+    half const *alpha = st->ctx;
     Half X = cast(Half, x),
          Y = cast(Half, y);
     return (RGBA){
@@ -25,3 +27,115 @@ define_step(grad) {
         splat(Half, *alpha),
     };
 }
+
+CC RGBA src(RGBA s, RGBA d) {
+    (void)d;
+    return s;
+}
+
+CC RGBA srcover(RGBA s, RGBA d) {
+    s.r += d.r * (1-s.a);
+    s.g += d.g * (1-s.a);
+    s.b += d.b * (1-s.a);
+    s.a += d.a * (1-s.a);
+    return s;
+}
+
+static Half lerp(Half from, Half to, Half t) {
+    return (to - from) * t + from;
+}
+
+void blitrow(void* dst, int dx, int dy, int n,
+             struct DstFormat const *fmt,
+             struct Step            *cov,
+             struct Step            *src,
+             BlendFn                *blend) {
+    int const K = sizeof(Half) / sizeof(half);
+
+    union {
+        float arr[8];
+        Float vec;
+    } iota = {{0,1,2,3,4,5,6,7}};
+
+    Float       x = iota.vec       + (float)dx + 0.5f;
+    Float const y = splat(Float,0) + (float)dy + 0.5f;
+
+    while (n >= K) {
+        RGBA d = fmt->load(dst),
+             s = call(src,x,y),
+             b = blend(s,d),
+             c = call(cov,x,y),
+             l = {
+                 lerp(d.r, b.r, c.r),
+                 lerp(d.g, b.g, c.g),
+                 lerp(d.b, b.b, c.b),
+                 lerp(d.a, b.a, c.a),
+             };
+        fmt->store(l,dst);
+
+        dst = (char*)dst + K*fmt->bpp;
+        x  += (float)K;
+        n  -= K;
+    }
+    while (n >= 1) {
+        RGBA d = fmt->load1(dst),
+             s = call(src,x,y),
+             b = blend(s,d),
+             c = call(cov,x,y),
+             l = {
+                 lerp(d.r, b.r, c.r),
+                 lerp(d.g, b.g, c.g),
+                 lerp(d.b, b.b, c.b),
+                 lerp(d.a, b.a, c.a),
+             };
+        fmt->store1(l,dst);
+
+        dst = (char*)dst + 1*fmt->bpp;
+        x  += (float)1;
+        n  -= 1;
+    }
+}
+
+typedef vec(uint32_t) U32;
+
+static RGBA from_rgba_8888(U32 px) {
+    return (RGBA) {
+        cast(Half, cast(Float, (px >>  0) & 0xff)),
+        cast(Half, cast(Float, (px >>  8) & 0xff)),
+        cast(Half, cast(Float, (px >> 16) & 0xff)),
+        cast(Half, cast(Float, (px >> 24) & 0xff)),
+    };
+}
+static U32 to_rgba_8888(RGBA rgba) {
+    return cast(U32, rgba.r * 255.0f + 0.5f) <<  0
+         | cast(U32, rgba.g * 255.0f + 0.5f) <<  8
+         | cast(U32, rgba.b * 255.0f + 0.5f) << 16
+         | cast(U32, rgba.a * 255.0f + 0.5f) << 24;
+}
+
+static RGBA load_rgba_8888(void const *ptr) {
+    U32 px;
+    memcpy(&px, ptr, sizeof px);
+    return from_rgba_8888(px);
+}
+static RGBA load1_rgba_8888(void const *ptr) {
+    U32 px={0};
+    memcpy(&px, ptr, 4);
+    return from_rgba_8888(px);
+}
+static void store_rgba_8888(RGBA rgba, void *ptr) {
+    U32 px = to_rgba_8888(rgba);
+    memcpy(ptr, &px, sizeof px);
+}
+static void store1_rgba_8888(RGBA rgba, void *ptr) {
+    U32 px = to_rgba_8888(rgba);
+    memcpy(ptr, &px, 4);
+}
+
+struct DstFormat const rgba_8888 = {
+    4,
+    load_rgba_8888,
+    load1_rgba_8888,
+    store_rgba_8888,
+    store1_rgba_8888,
+};
--- simplest.h
+++ simplest.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <stddef.h>
 
 #if defined(__ARM_FEATURE_FP16_VECTOR_ARITHMETIC)
     typedef _Float16 half;
@@ -34,27 +35,49 @@ struct Step {
     void *ctx;
 };
 
+// TODO: xl,xh,yl,yh swizzling can't be right on both x86 and ARM.  Test, fix.
+
 #define declare_step(name) CC RGBA name(struct Step*, Half,Half,Half,Half)
 
 #define define_step(name)                                               \
-    static RGBA name##_(struct Step *ip, Float x, Float y);             \
-    CC RGBA name(struct Step *ip, Half xl, Half xh, Half yl, Half yh) { \
+    static RGBA name##_(struct Step *st, Float x, Float y);             \
+    CC RGBA name(struct Step *st, Half xl, Half xh, Half yl, Half yh) { \
         union {                                                         \
             Half  h[4];                                                 \
             Float f[2];                                                 \
         } xy = {{xl,xh,yl,yh}};                                         \
-        return name##_(ip, xy.f[0], xy.f[1]);                           \
+        return name##_(st, xy.f[0], xy.f[1]);                           \
     }                                                                   \
-    static RGBA name##_(struct Step *ip, Float x, Float y)
+    static RGBA name##_(struct Step *st, Float x, Float y)
 
-static inline RGBA next(struct Step *ip, Float x, Float y) {
+static inline RGBA call(struct Step *st, Float x, Float y) {
     union {
         Float f[2];
         Half  h[4];
     } abi = {{x,y}};
-    return ip[1].fn(ip+1, abi.h[0], abi.h[1], abi.h[2], abi.h[3]);
+    return st->fn(st, abi.h[0], abi.h[1], abi.h[2], abi.h[3]);
 }
 
 declare_step(noop);
 declare_step(swap_rb);
 declare_step(grad);
+
+typedef RGBA (CC BlendFn)(RGBA, RGBA);
+
+BlendFn src,srcover;
+
+struct DstFormat {
+    size_t bpp;
+    RGBA (*load  )(void const*);
+    RGBA (*load1 )(void const*);
+    void (*store )(RGBA, void*);
+    void (*store1)(RGBA, void*);
+};
+
+extern struct DstFormat const rgba_8888;
+
+void blitrow(void *dst, int dx, int dy, int n,
+             struct DstFormat const *fmt,
+             struct Step            *cov,
+             struct Step            *src,
+             BlendFn                *blend);
