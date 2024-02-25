274c7f854d3e4c2f32a36dc3d6201385213c3c3e
Mike Klein <mtklein@gmail.com>
2024-02-25 09:29:25 -0600

    just be normal

--- simplest.h
+++ simplest.h
@@ -46,30 +46,15 @@ typedef struct {
 } RGBA;
 
 struct Stage {
-    RGBA (CC *fn)(struct Stage*, Half,Half,Half,Half);
+    RGBA (CC *fn)(struct Stage*, Float,Float);
     void *ctx;
 };
 
-#define declare_stage(name) CC RGBA name(struct Stage*, Half,Half,Half,Half)
-
-#define define_stage(name)                                                \
-    declare_stage(name);                                                  \
-    AI static inline RGBA name##_(struct Stage st[], Float x, Float y);   \
-    CC RGBA name(struct Stage st[], Half xl, Half xh, Half yl, Half yh) { \
-        union {                                                           \
-            Half  h[4];                                                   \
-            Float f[2];                                                   \
-        } xy = {{xl,xh,yl,yh}};                                           \
-        return name##_(st, xy.f[0], xy.f[1]);                             \
-    }                                                                     \
-    AI static inline RGBA name##_(struct Stage st[], Float x, Float y)
+#define declare_stage(name) CC RGBA name(struct Stage st[], Float x, Float y)
+#define  define_stage(name) CC RGBA name(struct Stage st[], Float x, Float y)
 
 AI static inline RGBA call(struct Stage st[], Float x, Float y) {
-    union {
-        Float f[2];
-        Half  h[4];
-    } abi = {{x,y}};
-    return st->fn(st, abi.h[0], abi.h[1], abi.h[2], abi.h[3]);
+    return st->fn(st, x,y);
 }
 
 declare_stage(noop);
